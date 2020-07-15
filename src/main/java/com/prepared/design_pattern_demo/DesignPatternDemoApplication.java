378c5e15e66358cfde2b33900a20e8178397c9b2
Administrator <admin@example.com>
2020-07-15 14:54:09 +0800

    first commit

---
+++ src/main/java/com/prepared/design_pattern_demo/DesignPatternDemoApplication.java
@@ -0,0 +1,13 @@
+package com.prepared.design_pattern_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prepared/design_pattern_demo/DesignPatternDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prepared.design_pattern_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
